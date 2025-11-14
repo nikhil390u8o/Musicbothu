9e7429f52e2d333abf294c4f729dab7ee2cb938f
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-11-14 14:09:12 +0530

    Add configuration settings for the bot

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+API_ID = 12345678
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your:bot_token"
+SESSION_NAME = "musicbot"
+API_KEY = "xbYml37389QsJnEFKEPS"
+API_URL = "https://deadlinetech.site"
+DURATION_LIMIT = 1800  # 30 minutes
